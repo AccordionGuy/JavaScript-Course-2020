31fdc3f1a653363cdd35a91ba0203dc7b9bc078a
Joey deVilla <joey@joeydevilla.com>
2020-09-29 22:17:25 -0400

    Day 1

---
+++ hello_there.js
@@ -0,0 +1,7 @@
+console.log("Hello there!")
+console.log("Welcome to the program.")
+let name = "Joey"
+console.log(name)
+let sum = 56 + 78
+console.log(sum)
+console.log(`My name is ${123 * 345}`)
